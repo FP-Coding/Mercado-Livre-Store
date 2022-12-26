78cd9c6bc28abe39648544b0db8c6d57aae2c992
Felipe Pinto <felipe14062000@gmail.com>
2022-12-26 19:56:55 -0300

    feat: criando barra lateral de pesquisa por categoria

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from 'react';
-import { getItensByIdCategoryAndQuery } from '../../utils/api';
+import React, { useState, useEffect } from 'react';
+import {
+  getItensByIdCategoryAndQuery,
+  getAllCategories,
+  getItensByIdCategory
+} from '../../utils/api';
 import ProductCard from '../../components/ProductCard/ProductCard';
 // import PropTypes from 'prop-types';
 
 function Home() {
   const [query, setQuery] = useState('');
   const [items, setItems] = useState([]);
+  const [categories, setCategories] = useState([]);
   const [idCategorySearched, setIdCategorySearched] = useState('');
+
+  useEffect(() => {
+    const requestCategories = async () => {
+      const allCategories = await getAllCategories();
+      setCategories(allCategories);
+    };
+    requestCategories();
+  }, []);
 
   const submitSearch = async () => {
     const itemsSearched = await getItensByIdCategoryAndQuery(
@@ -16,6 +29,11 @@ function Home() {
     setItems(itemsSearched);
     setIdCategorySearched('');
   };
+
+  const filterById = async () => {
+    const getItems = await getItensByIdCategory(idCategorySearched);
+    setItems(getItems);
+  };
   return (
     <main>
       <section className="flex items-center justify-center gap-3 mt-5 h-12">
@@ -23,7 +41,7 @@ function Home() {
           type="text"
           value={query}
           onChange={({ target: { value } }) => setQuery(value)}
-          className="rounded w-[45%] h-full p-2 bg-slate-600 outline-none focus:border-purple-300 focus:border-2"
+          className="rounded w-2/3 h-full p-2 bg-slate-600 outline-none focus:border-purple-300 focus:border-2"
         />
         <button
           type="button"
@@ -33,36 +51,53 @@ function Home() {
           Pesquisar
         </button>
       </section>
-      <section className="mt-5">
-        {items.length < 1 ? (
-          <p className="text-center">
-            Digite algum termo de pesquisa ou escolha uma categoria.
-          </p>
-        ) : (
-          <div className="flex gap-2 flex-wrap m-4 items-center justify-center">
-            {items.map(
-              ({
-                id,
-                title,
-                thumbnail,
-                price,
-                available_quantity: stockQuantity,
-                shipping: { free_shipping: isShippingFree }
-              }) => (
-                <ProductCard
-                  key={id}
-                  id={id}
-                  name={title}
-                  image={thumbnail}
-                  price={price}
-                  stock={stockQuantity}
-                  isShippingFree={isShippingFree}
-                />
-              )
-            )}
-          </div>
-        )}
-      </section>
+      <div className="flex">
+        <aside className="flex flex-col mt-8 ml-8 p-4 w-60 rounded bg-gray-700">
+          {categories.map(({ name, id }) => (
+            <label htmlFor={id}>
+              <input
+                type="radio"
+                name="selectCategory"
+                value={name}
+                id={id}
+                onChange={({ target }) => setIdCategorySearched(target.id)}
+                onClick={filterById}
+              />
+              {name}
+            </label>
+          ))}
+        </aside>
+        <section className="mt-5">
+          {items.length < 1 ? (
+            <p className="text-center m-auto p-4">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </p>
+          ) : (
+            <div className="flex gap-2 flex-wrap m-4 items-center justify-center">
+              {items.map(
+                ({
+                  id,
+                  title,
+                  thumbnail,
+                  price,
+                  available_quantity: stockQuantity,
+                  shipping: { free_shipping: isShippingFree }
+                }) => (
+                  <ProductCard
+                    key={id}
+                    id={id}
+                    name={title}
+                    image={thumbnail}
+                    price={price}
+                    stock={stockQuantity}
+                    isShippingFree={isShippingFree}
+                  />
+                )
+              )}
+            </div>
+          )}
+        </section>
+      </div>
     </main>
   );
 }
