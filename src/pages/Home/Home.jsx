21db85d0ccc41f8e144a745d09b6cadb26d37fde
Felipe Pinto <felipe14062000@gmail.com>
2022-12-27 11:49:15 -0300

    fix: corrigindo responsividade do menu de categorias e bug de asincronicidade para mostrar os produtos

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,6 +12,7 @@ function Home() {
   const [items, setItems] = useState([]);
   const [categories, setCategories] = useState([]);
   const [idCategorySearched, setIdCategorySearched] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const requestCategories = async () => {
@@ -21,19 +22,26 @@ function Home() {
     requestCategories();
   }, []);
 
+  useEffect(() => {
+    setIsLoading(true);
+    const filterById = async () => {
+      const getItems = await getItensByIdCategory(idCategorySearched);
+      setItems(getItems);
+    };
+    filterById();
+    setIsLoading(false);
+  }, [idCategorySearched]);
+
   const submitSearch = async () => {
+    setIsLoading(true);
     const itemsSearched = await getItensByIdCategoryAndQuery(
       idCategorySearched,
       query
     );
     setItems(itemsSearched);
-    setIdCategorySearched('');
+    setIsLoading(false);
   };
 
-  const filterById = async () => {
-    const getItems = await getItensByIdCategory(idCategorySearched);
-    setItems(getItems);
-  };
   return (
     <main>
       <section className="flex items-center justify-center gap-3 mt-5 h-12">
@@ -51,8 +59,8 @@ function Home() {
           Pesquisar
         </button>
       </section>
-      <div className="flex">
-        <aside className="flex flex-col mt-8 ml-8 p-4 w-60 rounded bg-gray-700">
+      <div className="flex justify-center">
+        <aside className="hidden lg:flex flex-col mt-8 ml-8 p-4 w-[12%] rounded bg-gray-700">
           {categories.map(({ name, id }) => (
             <label htmlFor={id}>
               <input
@@ -61,42 +69,45 @@ function Home() {
                 value={name}
                 id={id}
                 onChange={({ target }) => setIdCategorySearched(target.id)}
-                onClick={filterById}
               />
               {name}
             </label>
           ))}
         </aside>
-        <section className="mt-5">
-          {items.length < 1 ? (
-            <p className="text-center m-auto p-4">
-              Digite algum termo de pesquisa ou escolha uma categoria.
-            </p>
-          ) : (
-            <div className="flex gap-2 flex-wrap m-4 items-center justify-center">
-              {items.map(
-                ({
-                  id,
-                  title,
-                  thumbnail,
-                  price,
-                  available_quantity: stockQuantity,
-                  shipping: { free_shipping: isShippingFree }
-                }) => (
-                  <ProductCard
-                    key={id}
-                    id={id}
-                    name={title}
-                    image={thumbnail}
-                    price={price}
-                    stock={stockQuantity}
-                    isShippingFree={isShippingFree}
-                  />
-                )
-              )}
-            </div>
-          )}
-        </section>
+        {!isLoading ? (
+          <section className="mt-5 w-2/3">
+            {items.length < 1 ? (
+              <p className="text-center m-auto p-4">
+                Digite algum termo de pesquisa ou escolha uma categoria.
+              </p>
+            ) : (
+              <div className="flex gap-2 flex-wrap m-4 items-center justify-center">
+                {items.map(
+                  ({
+                    id,
+                    title,
+                    thumbnail,
+                    price,
+                    available_quantity: stockQuantity,
+                    shipping: { free_shipping: isShippingFree }
+                  }) => (
+                    <ProductCard
+                      key={id}
+                      id={id}
+                      name={title}
+                      image={thumbnail}
+                      price={price}
+                      stock={stockQuantity}
+                      isShippingFree={isShippingFree}
+                    />
+                  )
+                )}
+              </div>
+            )}
+          </section>
+        ) : (
+          <p>Carregando</p>
+        )}
       </div>
     </main>
   );
